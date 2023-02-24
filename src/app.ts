ee871b116724f374b0d6e1eab27dff018984a05a
Aaron Chen <aaron.kst99@gmail.com>
2023-02-24 14:19:05 +0630

    Added @aws-sdk/client-ivs

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,34 @@
 import express from "express";
+import { IvsClient, CreateChannelCommand } from "@aws-sdk/client-ivs";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+// TODO: add authentication middleware logic
+app.post("/ivs", async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) return res.sendStatus(400);
+    const client = new IvsClient({
+      region: "ap-northeast-1",
+    });
+    const command = new CreateChannelCommand({
+      name,
+    });
+
+    // response.channel.ingestEndpoint
+    // response.channel.name
+    // response.channel.playbackUrl
+    // response.streamKey.value
+
+    const response = await client.send(command);
+    return res.json({ ...response });
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
 
 export { app };
